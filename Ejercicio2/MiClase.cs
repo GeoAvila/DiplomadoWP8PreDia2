bcb3d004c1de3a99f2f4932c2754d90dfd65cacc
GeoAvila <geovanny.avila@gmail.com>
2013-06-01 00:30:58 -0600

    Ejemplos Varios Dia 2

--- Ejercicio2/MiClase.cs
+++ Ejercicio2/MiClase.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace Ejercicio2
 {
-    class MiClase
+    public class MiClase
     {
-            int campo;
+        int campo;
+
+            public int CampoComoPropiedad
+            {
+                get { return campo; }
+                set { campo = value; }
+            }
             const int constante = 5;
+
             public MiClase()
             {
                 campo = 10;
             }
+
+            public MiClase(int valor)
+            {
+                campo = valor;
+            }
+
             int miPropiedadInterna;
-            int miPropiedad
+            public int miPropiedad
             {
                 get {
                     return miPropiedadInterna;
@@ -25,15 +38,30 @@ namespace Ejercicio2
                     miPropiedadInterna = value;
                 }
             }
-            private int MiMetodo(int parametro,string otroParemetro)
+            public int MiMetodo(ref int parametro,string otroParemetro,out double parametro3)
             {
+                parametro = 10;
+                parametro3 = 99.1;
                 return 0;
             }
             int[] indexador;
-            public event EventHandler MiEvento;
+            public static event EventHandler MiEvento;
             public int this[int valor]
             {
                 get { return indexador[valor]; }
+            }
+
+            public int Metodo()
+            {
+                return 0;
+            }
+            public int Metodo(int valor)
+            {
+                return 1;
+            }
+            public int MetodoDiferente(int valor, string a, double y)
+            {
+                return 5;
             }
                
     }   
--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -3,15 +3,73 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Ejercicio2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MiClase obj = new MiClase();
+            MiClase obj2 = new MiClase(20) { miPropiedad = 20, CampoComoPropiedad=10};
+            
+            var nombre = args.SingleOrDefault();
+
+            //ejemplo IF
+            /*  if (nombre!= null && nombre == "jluis")
+            { 
+            
+            }*/
+
+            //ejemplo Switch
+            /*switch (args.SingleOrDefault())
+            {
+                case "jluis":
+                    {
+                        Console.WriteLine("hola jluis!!!");
+                        break;
+                    }
+                case "geoavila":
+                    {
+                        Console.WriteLine("Hola Geo");
+                        break;
+                    }
+
+                default:
+                    Console.WriteLine("hola!!!");
+                    break;
+            }*/
+
+            //ejemplo While
+           /* var bandera = 10;
+            while (bandera > 0)
+            {
+                Console.WriteLine(bandera);
+                bandera--;
+            }*/
+
+            //ejemplo DO
+            /*var bandera = 10;
+            do
+            {
+                Console.WriteLine(bandera);
+                bandera--;
+            } while (bandera > 0);*/
+
+            //ejemplo FOR
+           /* for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            //ejemplo ForEach
+            int[] array = new int[10]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+            int[,] new_array =  new int[2,2] {{1,2},{3,4}};
 
 
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
