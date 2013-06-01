467a9ba1700bc4d39a35b54a716ca6317fd72809
GeoAvila <geovanny.avila@gmail.com>
2013-05-31 23:17:08 -0600

    Master

---
+++ Ejercicio2/MiClase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class MiClase
+    {
+            int campo;
+            const int constante = 5;
+            public MiClase()
+            {
+                campo = 10;
+            }
+            int miPropiedadInterna;
+            int miPropiedad
+            {
+                get {
+                    return miPropiedadInterna;
+                }
+                set
+                {
+                    miPropiedadInterna = value;
+                }
+            }
+            private int MiMetodo(int parametro,string otroParemetro)
+            {
+                return 0;
+            }
+            int[] indexador;
+            public event EventHandler MiEvento;
+            public int this[int valor]
+            {
+                get { return indexador[valor]; }
+            }
+               
+    }   
+}
